cb0b3dba8d90e0e8d0b75fe05f6237746eedbc6d
kerikir <vlasov-kerik@mail.ru>
2026-01-02 23:59:25 +0300

    bugs in calculations have been fixed

--- src/main/kotlin/com/lessons/first/EquationWithRoot.kt
+++ src/main/kotlin/com/lessons/first/EquationWithRoot.kt
@@ -1,5 +1,10 @@
 package com.lessons.first
 
+
+/*
+Время = 146ms
+Память = 12.38Mb
+ */
 fun main() {
     val a = readLine()!!.toInt()
     val b = readLine()!!.toInt()
@@ -14,14 +19,16 @@ fun calculateEquation(a: Int, b: Int, c: Int) : String {
     // Область допустимых значений
     if (c < 0) return "NO SOLUTION"
 
-    if (b == c && a == 0) {
+    if (b == c * c) {
         return if (a == 0) "MANY SOLUTIONS" else "0"
     }
+
+    if (a == 0) return "NO SOLUTION"
 
     val xDouble = (c * c - b) / a.toDouble()
     val xInt = (c * c - b) / a
 
-    if ((a * xInt + b) < 0) return "NO SOLUTION"
+    if ((a * xDouble + b) < 0) return "NO SOLUTION"
 
     if (xDouble == xInt.toDouble()) {
         return xInt.toString()
